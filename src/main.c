95e529a868f87584620bd4022bcb68c36968f3c5
Phil Erwin <phil.erwin@lexmark.com>
2024-02-03 10:51:11 -0500

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Phil Erwin
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+   printk("Hello World from Seeed Wio-wm1110 !!! \n");
+
+   return 0;
+}
